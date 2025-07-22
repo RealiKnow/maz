e8071ac733dab9e57f7ed7738c26d35e5a44af18
RealiKnow <itsrealiknow@gmail.com>
2025-07-22 09:26:44 +0200

    Create login.js

---
+++ backup/js/login.js
@@ -0,0 +1,29 @@
+// login.js
+const firebaseConfig = {
+    apiKey: "AIzaSyD2e5ZFUMY67KJsUrrflJpSIm1UpE4gn_Q",
+    authDomain: "realauth-f47e3.firebaseapp.com",
+    projectId: "realauth-f47e3",
+    storageBucket: "realauth-f47e3.appspot.com",
+    messagingSenderId: "246570204070",
+    appId: "1:246570204070:web:d89596de8b627467e66d67"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+
+document.getElementById('login-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    
+    auth.signInWithEmailAndPassword(email, password)
+        .then((userCredential) => {
+            // Redirect to chat page
+            window.location.href = 'chat.html';
+        })
+        .catch((error) => {
+            alert("Login failed: " + error.message);
+        });
+});
